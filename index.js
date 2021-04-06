e41cca67d7cff15b0a65335bc550b10d77c22ca7
Casey D <caseydasilva@hotmail.com>
2021-04-05 17:48:20 -0700

    fixed minor bug
    the onClick event listener was only working if the img was clicked from the start; so if you loaded a pic, did not click it to enlarge the img, then loaded another pic, you could enlarge the newly loaded pic but the previous pic would only load if it had already been enlarged. 
    now the imgs all respond to the click EListener

--- index.js
+++ index.js
@@ -11,31 +11,21 @@ const fetchData = async (searchTerm) => {
     return response.data
 };
 
-const input = document.querySelector('input');
-const onInput = async e => {
-    const data = await fetchData(e.target.value)
-    const searchResults = document.createElement('div');
-    const imgSrc = data.hdurl === undefined ? data.url : data.hdurl;
-    const title = data.title;
-    const desc = data.explanation;
-    displayResults(searchResults, imgSrc, title, desc);
-};
-input.addEventListener('input', debounce(onInput, 300));
-
-const pageDisplay = document.querySelector('#searchResultsFill')
-pageDisplay.addEventListener('click', e => {    
-    const img = document.querySelector('img');
-    const recallImgSrc = img.src;
-    expandImg(img, recallImgSrc)
-})
-
-function displayResults(content, img, title, desc) {
+const displayResults = (content, src, title, desc) => {
     content.innerHTML = `
             <div class="box">
                 <article class="media">
                     <div class="media-left">
                         <figure class="image is-128x128">
-                            <img src="${img}" alt="image"/>
+                        <img src="${src}" alt="image" class="thumbnail-image"/>
+                            <div class="modal">
+                                <div class="modal-background"></div>
+                                <div class="modal-content">
+                                    <p class="image is-6by8">
+                                        <img src="${src}" alt="image" class="modal-image" />
+                                    </p>
+                                </div>
+                            
                         </figure>
                     </div>
                     <div class="media-content">
@@ -50,27 +40,26 @@ function displayResults(content, img, title, desc) {
             </div>
             <br>
             `;
-    document.querySelector('#searchResultsFill').insertAdjacentElement('afterbegin', content); 
+    document.querySelector('#searchResultsFill').insertAdjacentElement('afterbegin', content);
+    
+    const modal = content.querySelector('.modal')
+    const img = content.querySelector('img')
+    document.addEventListener('click', (e) => {
+        if (!img.contains(e.target)) {
+            modal.classList.remove('is-active');
+        } else {
+            modal.classList.add('is-active')
+        }
+    });
 };
 
-function expandImg(item, src) {
-    const modal = document.createElement('div');
-    const img = document.querySelector('img')
-    modal.classList.add('modal');
-    document.addEventListener('click', e => {
-        if (img.contains(e.target)) {
-           modal.classList.add('is-active')
-        } else {
-            modal.classList.remove('is-active')
-       }
-    });
-    modal.innerHTML = `
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            <p class="image is-10by12">
-            <img src="${src}" alt="">
-            </p>
-        </div>
-`;
-    item.insertAdjacentElement('beforebegin', modal)
-}
+const input = document.querySelector('input');
+const onInput = async e => {
+    const data = await fetchData(e.target.value)
+    const searchResults = document.createElement('div');
+    const imgSrc = data.hdurl === undefined ? data.url : data.hdurl;
+    const title = data.title;
+    const desc = data.explanation;
+    displayResults(searchResults, imgSrc, title, desc);
+};
+input.addEventListener('input', debounce(onInput, 300));
